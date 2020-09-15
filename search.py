303217030c93b5f8dd2d3ee03aea9b6c4437e19a
Ivana Pavlovic <56420087+frauiv@users.noreply.github.com>
2020-09-15 08:53:56 -0500

    bfs works

--- search.py
+++ search.py
@@ -111,6 +111,50 @@ def search_dfs(problem, object):
                     object.push([moveNum+1, successor])
     return []
 
+def search_bfs(problem, object):
+    discovered = []
+    object.push([-1,(problem.getStartState(), "Stop", 0)])
+    possible_states = []
+    list_of_actions =[]
+    # We will use queue and a list
+    while (not object.isEmpty()):
+        state = object.pop()
+        #get the current state location and parent location in list
+        state_location = state[1][0]
+        parent_list_index = state[0]
+
+        if problem.isGoalState(state_location):
+            # print list_of_actions
+            # just the action
+            possible_states.append((parent_list_index, state[1][1]))
+            list_of_actions.append(state[1][1])
+            while(parent_list_index != 0):
+                state_to_add = possible_states[parent_list_index]
+                list_of_actions.append(state_to_add[1])
+                parent_list_index = state_to_add[0]
+            list_of_actions.reverse()
+            
+            return list_of_actions
+
+        #only will add successors and append to list if current location not discovered 
+        if state_location not in discovered:
+            #add current satate to discovered list
+            discovered.append(state_location)
+            
+            #append parents location in possible state list 
+            #alpng with the action state and state location on board 
+            possible_states.append((parent_list_index, state[1][1]))
+
+            #get location of parent in the possible states list 
+            parent_list_index = len(possible_states)-1
+
+            #get successors and add them to queue 
+            for successor in problem.getSuccessors(state_location):
+                if successor[0] not in discovered:
+                    #only append parent location in possible states and the successor 
+                    object.push([parent_list_index,successor])
+    return []
+
 def depthFirstSearch(problem):
     # Initialize an empty Stack
     object = util.Stack()
